222bdf64c16cd134a4ca040c1ad55f610d146e7a
RafaJ06 <juanrafaelramirezmontero2@gmail.com>
2025-01-08 19:30:13 -0400

    Creando y añadiendo una solución y sus programas

---
+++ SolucionPrincipal/ClaseUno/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseUno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Este es el código de Juan");
+            Console.Write("Estoy usando GitHub y Git");  
+            Console.ReadLine();
+        }
+    }
+}
