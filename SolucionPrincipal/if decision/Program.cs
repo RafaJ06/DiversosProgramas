b58acb39c6ec9eaa063fd5a68a148db31b5975fa
RafaJ06 <juanrafaelramirezmontero2@gmail.com>
2025-01-14 08:46:26 -0400

    Utilizando decisiones (if else), primera parte

---
+++ SolucionPrincipal/if decision/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace if_decision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //first part
+            Console.WriteLine("Bienvenido a las casa de Juan ¡El magnifico!");
+            Console.WriteLine("Elije una puerta a tomar");
+            string valorUsuario = Console.ReadLine();   
+
+            if (valorUsuario == "1"){
+
+                string mensaje = "Mala elección, ahora me debes todo tu dinero";
+                Console.WriteLine(mensaje);}
+
+            //Agregando más opciones
+
+            if (valorUsuario == "2") {
+
+                string mensaje = "Mala elección, ahora tu casa es mía";
+                Console.WriteLine(mensaje);
+            }
+
+            if (valorUsuario == "3")
+            {
+
+                string mensaje = "Buena elección ¡Te puedes ir de mi casa!";
+                Console.WriteLine(mensaje);
+            }
+
+
+            Console.ReadLine();
+        }
+
+       
+    }
+}
