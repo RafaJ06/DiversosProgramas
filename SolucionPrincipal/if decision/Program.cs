f5f125d597161eb36c800a725a0699c1b4e308fb
RafaJ06 <juanrafaelramirezmontero2@gmail.com>
2025-01-14 09:33:52 -0400

    Mejorando y añadiendo más formas de optimmizar código

--- SolucionPrincipal/if decision/Program.cs
+++ SolucionPrincipal/if decision/Program.cs
@@ -10,31 +10,45 @@ namespace if_decision
     {
         static void Main(string[] args)
         {
-            //first part
+            //    //first part
+            //    Console.WriteLine("Bienvenido a las casa de Juan ¡El magnifico!");
+            //    Console.WriteLine("Elije una puerta a tomar");
+            //    string valorUsuario = Console.ReadLine();
+
+            //    string mensaje = ""; 
+
+            //    if (valorUsuario == "1") mensaje = "Mala elección, ahora me debes todo tu dinero";
+
+            //    //Agregando más opciones
+
+            //    else if (valorUsuario == "2") mensaje = "Mala elección, ahora tu casa es mía";
+
+            //    else if (valorUsuario == "3") mensaje = "Buena elección ¡Te puedes ir de mi casa!";
+
+            //    else
+            //    {
+            //        mensaje = "Entra por una puerta correcta, por favor";
+            //        mensaje += ". Si no tomas un apuerta no podras entrar y ver mi hermosa casa";
+            //    }
+
+            //    Console.WriteLine(mensaje);
+            //    Console.ReadLine();
+
             Console.WriteLine("Bienvenido a las casa de Juan ¡El magnifico!");
             Console.WriteLine("Elije una puerta a tomar");
             string valorUsuario = Console.ReadLine();
 
-            string mensaje = ""; 
+            string mensaje = (valorUsuario == "1") ? "bote" : "Selecciona una opción correcta";
 
-            if (valorUsuario == "1") mensaje = "Mala elección, ahora me debes todo tu dinero";
+            //Console.Write("Ganaste un ");
+            //Console.Write(mensaje);
+            //Console.Write(".");
 
-            //Agregando más opciones
-
-            else if (valorUsuario == "2") mensaje = "Mala elección, ahora tu casa es mía";
-
-            else if (valorUsuario == "3") mensaje = "Buena elección ¡Te puedes ir de mi casa!";
-
-            else
-            {
-                mensaje = "Entra por una puerta correcta, por favor";
-                mensaje = mensaje + ". Si no tomas un apuerta no podras entrar y ver mi hermosa casa";
-            }
-
-            Console.WriteLine(mensaje);
+            Console.WriteLine("Elejiste la puerta {0}, por eso, Ganaste un {0}.",valorUsuario, mensaje);
             Console.ReadLine();
         }
 
-       
+
+
     }
 }
