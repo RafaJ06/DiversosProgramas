0e8ca195b4ec7d0659012d53cf76324634d9ca13
RafaJ06 <juanrafaelramirezmontero2@gmail.com>
2025-01-14 09:53:31 -0400

    Añadiendo tercera clase: Tipos de Datos y Variables

---
+++ SolucionPrincipal/TiposDV/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiposDV
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  int X;
+              int Y;
+
+              X = 7;
+              Y = X + 5;
+
+              Console.WriteLine(Y);*/
+              
+
+
+            Console.WriteLine("Dime tu nombre");
+            Console.Write("Dime tu primer nombre:");
+            string primerNombre;
+            primerNombre = Console.ReadLine();
+
+            string apellido;
+            Console.Write("Dime tu apellido:");
+            apellido = Console.ReadLine();
+
+            Console.WriteLine("¡Hey! Mucho gusto " + primerNombre + " " + apellido);
+            Console.ReadLine();
+        }
+    }
+}
